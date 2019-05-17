14860173b72618816dd5bc20f9e301a9c1bb46ac
0strich <gee8195@naver.com>
2019-05-18 04:23:10 +0900

    udated at 5/18

--- blank/Week 02/7-3.cpp
+++ blank/Week 02/7-3.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 class Student{
-	________________;							// 정적 멤버 변수 m_count 를 int형으로 선언
+	static int m_count;							// 정적 멤버 변수 m_count 를 int형으로 선언
 	string m_gender;
 	int m_age;
 public:
 	// 매개변수들의 디폴트 인수 값을 gender 는 NONE, age 는 20으로 세팅
-	Student(_________________________);
+	Student(string gender="NONE", int age=20);
 	~Student();									// 소멸자 함수
 	Student(const Student &s);					// 복사 생성자 함수
 	int Get_Age() { return m_age; }
-	string Get_Gender() { return m_gender }
+	string Get_Gender() { return m_gender; }
 };
 
 Student::Student(string gender, int age){
@@ -36,26 +36,26 @@ Student::~Student(){
 
 void Show_Student(Student s, string str){
 	cout << "\n--- Show_Student() 함수 시작 ---\n";
-	//
-	cout << str << "객체는 (" << ________________ << ")입니다.\n";
-	//
-	cout << "나이는 " << ________________ << "살 입니다.\n";
-	cout << --- Show_Student() 함수 종료 ---\n\n";
+	// ob1 의 객체의 성별 출력
+	cout << str << "객체는 (" << s.Get_Gender() << ")입니다.\n";
+	// ob2 의 객체의 성별 출력
+	cout << "나이는 " << s.Get_Gender() << "살 입니다.\n";
+	cout << "--- Show_Student() 함수 종료 ---\n\n";
 }
 
-_________________________ // 정적 멤버 변수(m_count) 메모리 할당(0으로 초기화)
+int Student::m_count = 0;// 정적 멤버 변수(m_count) 메모리 할당(0으로 초기화)
 
 int main(){
-	________________;						// Student 클래스로 ob1 객체 생성(초기화 "남학생", 22)
-	________________;						// Student 클래스로 ob2 객체 생성(초기화 "여핛애", 21)
-	________________;						// Student 클래스로 ob3 객체 생성(초기화 없음)
+	Student ob1("남학생", 22);						// Student 클래스로 ob1 객체 생성(초기화 "남학생", 22)
+	Student ob2("여학생", 21);						// Student 클래스로 ob2 객체 생성(초기화 "여학생", 21)
+	Student ob3;						// Student 클래스로 ob3 객체 생성(초기화 없음)
 	cout << "\n------ Main() 함수 시작 ------\n";
-	//
-	cout << "ob1 객체의 성별은 (" << ________________ << ")입니다.\n";
+	// ob1 객체의 성별 출력
+	cout << "ob1 객체의 성별은 (" << ob1.Get_Gender() << ")입니다.\n";
 	Show_Student(ob1, "ob1");
 
-	//
-	cout << "ob2 객체의 성별은 (" << ________________ << ")입니다\n";
+	// ob2 객체의 성별 출력
+	cout << "ob2 객체의 성별은 (" << ob2.Get_Gender() << ")입니다\n";
 	Show_Student(ob2, "ob2");
 
 	Show_Student(ob3, "ob3");
--- blank/Week 02/7-4.cpp
+++ blank/Week 02/7-4.cpp
@@ -2,28 +2,24 @@
 using namespace std;
 
 class myclass{
-	_____________________;							// 정적 멤버 변수 m_data 를 int 형으로 선언
+	static int m_data;							// 정적 멤버 변수 m_data 를 int 형으로 선언
 public:
 	void SetData(int n) { m_data = n; }
 	int GetData() { return m_data; }
 };
 
-_____________________								// 정젖 멤버 변수(m_data) 메모리 할당
-
-
+int myclass::m_data;// 정적 멤버 변수(m_data) 메모리 할당
 
 int main(){
 	myclass ob1, ob2;
 
+	ob1.SetData(100);							// 객체 ob1의 m_data 에 100의 값을 셋팅
 
-	_____________________;							// 객체 ob1의 m_data 에 100의 값을 셋팅
-
-	// ob1 의 data 값을 출력 
-	cout << "ob1의 m_data 값은 " << _____________________ << "\n";
+	// ob1 의 data 값을 출력
+	cout << "ob1의 m_data 값은 " << ob1.GetData() << "\n";
 
 	// ob2 의 data 값을 출력
-	cout << "ob2의 m_data 값은 " << _____________________ << "\n";
-	
+	cout << "ob2의 m_data 값은 " << ob2.GetData() << "\n";
+
 	return 0;
 }
-
